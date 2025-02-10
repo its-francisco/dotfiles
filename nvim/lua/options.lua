c6cea20be815ae220c6c2cffeb9ee89382db0747
Francisco <francisco.fernandes.2004@gmail.com>
2025-02-10 15:53:40 +0000

    initial commit

---
+++ nvim/lua/options.lua
@@ -0,0 +1,28 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 999
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.autoindent = true
+
+-- CTRL-Q to enter visual block mode
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+vim.g.mapleader = " "
